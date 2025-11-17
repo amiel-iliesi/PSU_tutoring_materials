06774176053cae6669ab458dcf030d11e813f7ad
Amiel Iliesi <iliesi.amiel@gmail.com>
2025-11-17 08:00:33 -0800

    made user addition of algorithms easier; improved output formatting

--- c++/algorithms/sorts/main.cpp
+++ c++/algorithms/sorts/main.cpp
@@ -1,16 +1,21 @@
 #include "bubble.h"
 #include "insertion.h"
+#include "merge.h"
 
 #include <iostream>
 #include <algorithm>
 #include <random>
 #include <vector>
+#include <chrono>
+#include <functional>
 
 using namespace std;
 
 // - helper function prototypes -------------------------------
 bool is_sorted(const vector<int>&);
 void display(const vector<int>&);
+chrono::microseconds time(vector<int>&, function<void(vector<int>&)>);
+void run(vector<int>&, const string&, function<void(vector<int>&)>);
 
 
 
@@ -22,7 +27,7 @@ int main()
 	mt19937 gen(rd());
 	
 	// - shuffle an ascending vector from [0, SIZE) ---------------
-	int SIZE = 16 + gen() % 8;
+	int SIZE = 512 + gen() % 512;
 
 	vector<int> shuffled_vector;
 	shuffled_vector.reserve(SIZE);
@@ -36,18 +41,24 @@ int main()
 	// - calls begin here -----------------------------------------
 	vector<int> v = shuffled_vector;
 
-	cout << "initial vector:" << endl;
-	display(v);
+	cout << "vector size is " << v.size() << endl;
 
 	cout << '\n';
 
-	bubble_sort(v);
-	cout << "bubble sort: " << (is_sorted(v) ? "✓" : "✗") << endl;
-	
-	v = shuffled_vector;
+	cout << "running sorts:\n--------------" << endl;
 
-	insertion_sort(v);
-	cout << "insertion sort: " << (is_sorted(v) ? "✓" : "✗") << endl;
+	// put sorts to be called here:
+	vector<pair<string, function<void(vector<int>&)>>> to_run = {
+		{"std::sort", ranges::sort},
+		{"bubble sort", bubble_sort},
+		{"insertion sort", insertion_sort},
+		{"merge sort", merge_sort}
+	};
+
+	for (auto &[name, func]: to_run) {
+		run(v, name, func);
+		v = shuffled_vector; //reset v for next run
+	}
 
 	return 0;
 }
@@ -84,4 +95,29 @@ void display(const vector<int>& v)
 	}
 
 	cout << endl;
+}
+
+chrono::microseconds time(vector<int>& v, function<void(vector<int>&)> f)
+{
+	chrono::high_resolution_clock clock;
+
+	auto t1 = clock.now();
+	f(v);
+	auto t2 = clock.now();
+
+	return duration_cast<chrono::microseconds>(t2 - t1);
+}
+
+void run(vector<int>& v, const string& f_name, function<void(vector<int>&)> f)
+{
+	chrono::microseconds runtime = time(v, f);
+	
+	if (is_sorted(v)) {
+		cout << "[✓] " << f_name << ":"
+			 << setw(21 - f_name.length()) << runtime << endl;
+	}
+	else { // if it's not sorted correctly, don't bother displaying the time
+		cout << "[✗] " << f_name << endl;
+	}
+
 }
---
+++ c++/algorithms/sorts/merge.cpp
@@ -0,0 +1,51 @@
+#include "merge.h"
+#include <vector>
+
+using namespace std;
+
+vector<int> merge(const vector<int>&, const vector<int>&);
+
+void merge_sort(vector<int>& v)
+{
+	if (v.size() <= 1) {
+		return;
+	}
+
+	auto middle = v.begin() + v.size() / 2;
+
+	vector<int> vl(v.begin(), middle);
+	vector<int> vr(middle, v.end());
+
+	merge_sort(vl);
+	merge_sort(vr);
+
+	v = merge(vl, vr);
+}
+
+vector<int> merge(const vector<int>& vl, const vector<int>& vr)
+{
+	auto vl_it = vl.begin();
+	auto vr_it = vr.begin();
+
+	vector<int> v;
+	v.reserve(vl.size() + vr.size());
+
+	while (vl_it != vl.end() and vr_it != vr.end()) {
+		if (*vl_it < *vr_it) {
+			v.push_back(*(vl_it++));
+		}
+		else {
+			v.push_back(*(vr_it++));
+		}
+	}
+
+	while (vl_it != vl.end()) {
+		v.push_back(*(vl_it++));
+	}
+
+	while (vr_it != vr.end()) {
+		v.push_back(*(vr_it++));
+	}
+
+	return v;
+}
