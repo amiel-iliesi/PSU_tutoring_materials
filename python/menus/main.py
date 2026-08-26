b6d47b22c452f466eebb429d6e404a98216cd4a5
Amiel Iliesi <iliesi.amiel@gmail.com>
2026-08-25 18:31:13 -0700

    removed a constant that should be opaque

--- python/menus/main.py
+++ python/menus/main.py
@@ -2,9 +2,8 @@ import good_menu as gm
 import bad_menu as bm
 
 USE_BAD = False
-MENU = bm.Menu if USE_BAD else gm.Menu
 
 if __name__ == '__main__':
-    menu = MENU()
+    menu = bm.Menu() if USE_BAD else gm.Menu()
 
     menu.main()
